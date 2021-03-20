b31aa676d41fc0eff40a6c109ecca18d1bd88b78
Rogino <roginoapacible@gmail.com>
2021-03-19 20:21:44 -0400

    first commit

--- PowerShellSample.ps1
+++ PowerShellSample.ps1
@@ -3,7 +3,7 @@ $date = Get-Date -Format MMMM/dd/yyyy
 $time = Get-date -format hh:mm:tt
 Write-Host "Today is $date, time is $time"
 Write-Host "Hello $env:COMPUTERNAME, This is your first PowerShell Core Script"
-Write-host "Added update to the repo"
+Write-host "Added update to the repository"
 
 #Get-process | Select-Object name,PM,SI
 #Get-process -Module | format-table
